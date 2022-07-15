7bfda52d6deb17c1f1066b3ab137263f8b73150b
GustavoOM <gumartins2001@gmail.com>
2022-07-15 08:34:33 -0300

    Adicionando music.wav e SomController.java

---
+++ src/Controler/SomController.java
@@ -0,0 +1,37 @@
+package Controler;
+
+import java.io.File;
+import javax.sound.sampled.*;
+
+public class SomController {
+
+    public void AudioCore() {
+        try {
+            //URL do som que no caso esta no pendrive, mais ainda é uma fase de teste.
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sons/music.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY); //Para repetir o som.
+        } catch (Exception ex) {
+
+            System.out.println("Erro ao executar SOM!");
+            ex.printStackTrace();
+        }
+    }
+
+    public static void tocarAudio(String nomeDoAudio) {
+        try {
+            Thread.sleep(50);
+            //URL do som que no caso esta no pendrive, mais ainda é uma fase de teste.
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sons/"+nomeDoAudio).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception ex) {
+
+            System.out.println("Erro ao executar SOM!");
+            ex.printStackTrace();
+        }
+    }
+}
