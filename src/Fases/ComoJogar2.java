9703e8a99204e7c4c629e28ecc6eba8cc729754b
GustavoOM <gumartins2001@gmail.com>
2022-07-15 09:35:44 -0300

    Adição de arquivos para a tela como jogar

---
+++ src/Fases/ComoJogar2.java
@@ -0,0 +1,18 @@
+package Fases;
+
+import Controler.ControleDeJogo;
+import Controler.MatrizObjetos;
+import Modelo.ParteMenu;
+
+public class ComoJogar2 {
+    public static void setMatrizParaComoJogar1() {
+
+        for(int i = 10; i >= 0; i--){
+            for(int j = 14; j >= 0; j--){
+                MatrizObjetos.setMatrizDeObjetos(10-i,14-j, new ParteMenu("comoJogar2/"+((15*i+j)+1)+".png"));
+                ((ParteMenu)(MatrizObjetos.getMatrizDeObjetos()[10-i][14-j])).setPosicaoInMenu(10-i,14-j);
+            }
+        }
+        ControleDeJogo.atualizarRecord(0);
+    }
+}
